7d7e49648561fad803651e7f9d10fe584d7f8e6b
KilyongJeong <aceppin@motizensoft.com>
2021-06-03 10:04:05 +0900

    .

--- JavaLab/src/ch13/FileToFile.java
+++ JavaLab/src/ch13/FileToFile.java
@@ -1,5 +1,7 @@
 package ch13;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * 파일(output.txt) -> 파일(output2.txt)
@@ -8,7 +10,17 @@ package ch13;
  */
 public class FileToFile {
 	public static void main(String[] args) {
-		
+		int readData = 0;
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream("output.txt");
+			while((readData = System.in.read()) != -1) {
+				fos.write(readData);
+			}
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
