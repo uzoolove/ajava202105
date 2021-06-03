4042674564c7ad0b594fe7f747524942cbfcc7a8
KilyongJeong <aceppin@motizensoft.com>
2021-06-03 09:54:46 +0900

    .

--- JavaLab/src/ch13/KeyboardToConsole.java
+++ JavaLab/src/ch13/KeyboardToConsole.java
@@ -1,5 +1,7 @@
 package ch13;
 
+import java.io.IOException;
+
 /**
  * 표준입력장치(키보드)에서 읽은 내용을 표준출력장치(콘솔)에 출력한다.
  * @author student
@@ -7,7 +9,14 @@ package ch13;
  */
 public class KeyboardToConsole {
 	public static void main(String[] args) {	
-		
+		int readData = 0;
+		try {
+			while((readData = System.in.read()) != -1) {
+				System.out.write(readData);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 }
 
