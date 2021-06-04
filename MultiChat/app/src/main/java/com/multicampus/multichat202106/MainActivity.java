d42a03b7ecc728911a73fe24109eb166b24e3941
KilyongJeong <aceppin@motizensoft.com>
2021-06-04 11:22:48 +0900

    .

--- MultiChat/app/src/main/java/com/multicampus/multichat202106/MainActivity.java
+++ MultiChat/app/src/main/java/com/multicampus/multichat202106/MainActivity.java
@@ -3,6 +3,8 @@ package com.multicampus.multichat202106;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.enterBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ChatActivity 호출
+                
+            }
+        });
     }
 }
