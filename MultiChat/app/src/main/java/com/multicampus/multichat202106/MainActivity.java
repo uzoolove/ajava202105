98af455b4082a1a8e015686040e76e3696099b3e
KilyongJeong <aceppin@motizensoft.com>
2021-06-04 11:45:53 +0900

    .

--- MultiChat/app/src/main/java/com/multicampus/multichat202106/MainActivity.java
+++ MultiChat/app/src/main/java/com/multicampus/multichat202106/MainActivity.java
@@ -2,9 +2,11 @@ package com.multicampus.multichat202106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +18,17 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ChatActivity 호출
+                String server = ((TextView)findViewById(R.id.server)).getText().toString();
+                String nickname = ((TextView)findViewById(R.id.nickname)).getText().toString();
+                System.out.println(server);
+                System.out.println(nickname);
 
+                // ChatActivity 호출
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                intent.putExtra("server", server);
+                intent.putExtra("nickname", nickname);
+
+                startActivity(intent);
             }
         });
     }
